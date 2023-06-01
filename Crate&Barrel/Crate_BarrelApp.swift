7ebba0d80abb6c78e0edea3851365b3551a497c7
Jonathan Ruiz <refactoredd@icloud.com>
2023-06-01 16:41:37 -0700

    Initial Commit

---
+++ Crate&Barrel/Crate_BarrelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crate_BarrelApp.swift
+//  Crate&Barrel
+//
+//  Created by Jonathan Ruiz on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Crate_BarrelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
